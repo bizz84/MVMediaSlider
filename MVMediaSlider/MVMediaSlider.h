34f8696a4a2505e7ecee055711e446c83f2a02a5
Bizzotto, Andrea <andrea.bizzotto@sky.uk>
2016-02-03 14:25:28 +0000

    Initial Commit

---
+++ MVMediaSlider/MVMediaSlider.h
@@ -0,0 +1,19 @@
+//
+//  MVMediaSlider.h
+//  MVMediaSlider
+//
+//  Created by Andrea Bizzotto on 03/02/2016.
+//  Copyright © 2016 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVMediaSlider.
+FOUNDATION_EXPORT double MVMediaSliderVersionNumber;
+
+//! Project version string for MVMediaSlider.
+FOUNDATION_EXPORT const unsigned char MVMediaSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVMediaSlider/PublicHeader.h>
+
+
